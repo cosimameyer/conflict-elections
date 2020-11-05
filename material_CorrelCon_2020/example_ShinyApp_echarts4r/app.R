99d41e6205c0684ffa85caca1de3a204caa6b304
Cosima Meyer <cosima@mobile-145-118.wlan.uni-mannheim.de>
2020-11-05 15:06:17 +0100

    Update files for CorrelCon

---
+++ material_CorrelCon_2020/example_ShinyApp_echarts4r/app.R
@@ -0,0 +1,87 @@
+# -------------------------------------------------------------------------
+# Topic: An example of how to create an easy ShinyApp with echarts4r
+# Name:
+# Date:
+# -------------------------------------------------------------------------
+
+
+# Load dependencies -------------------------------------------------------
+
+
+# Load dependencies
+library(shiny) # Web Application Framework for R
+library(echarts4r) # Create Interactive Graphs with 'Echarts JavaScript' Version 4
+library(magrittr) # A Forward-Pipe Operator for R
+library(dplyr) # A Grammar of Data Manipulation
+
+# We use the penguin data
+
+library(palmerpenguins) # Palmer Archipelago (Antarctica) Penguin Data
+
+
+# Define UI ---------------------------------------------------------------
+
+
+# Define UI for application that draws a scatter plot
+
+ui <- fluidPage(
+  # Application title
+  titlePanel("Penguins"),
+  
+  # Sidebar with check box
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput(
+        "penguinType",
+        h4("Select a penguin species"),
+        choices = list(
+          "Adelie" = "Adelie",
+          "Chinstrap" = "Chinstrap",
+          "Gentoo" = "Gentoo"
+        ),
+        selected = "Adelie"
+      )
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(echarts4rOutput("penguins"))
+  ))
+
+
+# Define server  ----------------------------------------------------------
+
+
+# Define server logic to draw a scatter plot
+server <- function(input, output) {
+  
+  output$penguins <- renderEcharts4r({
+    
+    # Prepare the data and add colors
+    penguins %<>%
+      dplyr::filter(species %in% input$penguinType) %>% 
+      dplyr::mutate(
+        color = case_when(
+          species == "Adelie" ~ "#35b1c9",
+          species == "Chinstrap" ~ "#ebc83d",
+          species == "Gentoo" ~ "#badf55"
+        )
+      ) 
+    
+    # Use echarts4r to generate a scatter plot where the flipper length is 
+    # plotted against weight
+    penguins %>%
+      group_by(year) %>%
+      e_charts(flipper_length_mm, timeline = TRUE) %>%
+      e_scatter(body_mass_g, symbol_size = 10) %>%
+      e_add("itemStyle", color) %>%
+      e_axis_labels(x = "Flipper \nlength", y = "Weight (in gram)")
+    
+  })
+}
+
+
+# Bring everything together -----------------------------------------------
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ material_CorrelCon_2020/example_ShinyApp_shinydashboard_ggplot/app.R
@@ -0,0 +1,82 @@
+# -------------------------------------------------------------------------
+# Topic: Shinydashboard: An example of how to create an easy ShinyApp with 
+#                        ggplot2
+# Name:
+# Date:
+# -------------------------------------------------------------------------
+
+# Load dependencies -------------------------------------------------------
+
+# Load dependencies
+library(shiny) # Web Application Framework for R
+library(shinydashboard) # Create Dashboards with 'Shiny'
+library(ggplot2)
+library(magrittr) # A Forward-Pipe Operator for R
+library(dplyr) # A Grammar of Data Manipulation
+
+# We use the penguin data
+library(palmerpenguins) # Palmer Archipelago (Antarctica) Penguin Data
+
+
+# Define UI ---------------------------------------------------------------
+
+
+# Define UI for application that draws a scatter plot
+
+ui <- dashboardPage(
+  # Application title
+  dashboardHeader(title = "Penguins"),
+  
+  # Sidebar with check box
+  dashboardSidebar(
+    checkboxGroupInput(
+      "penguinType",
+      h4("Select a penguin species"),
+      choices = list(
+        "Adelie" = "Adelie",
+        "Chinstrap" = "Chinstrap",
+        "Gentoo" = "Gentoo"
+      ),
+      selected = "Adelie"
+    )
+  ),
+  # Show a plot
+  dashboardBody(plotOutput("penguins"))
+)
+
+# Define server  ----------------------------------------------------------
+
+# Define server logic to draw a scatter plot
+server <- function(input, output) {
+  
+  output$penguins <- renderPlot({
+    
+    # Prepare the data and add colors
+    penguins %<>%
+      dplyr::filter(species %in% input$penguinType) %>% 
+      dplyr::mutate(
+        color = case_when(
+          species == "Adelie" ~ "#35b1c9",
+          species == "Chinstrap" ~ "#ebc83d",
+          species == "Gentoo" ~ "#badf55"
+        )
+      )
+    
+    # Use ggplot2 to generate a scatter plot where the flipper length is 
+    # plotted against weight
+    penguins %>%
+      ggplot(aes(
+        x = body_mass_g,
+        y = flipper_length_mm,
+        color = species)) +
+      geom_point(size = 10) +
+      labs(x = "Flipper \nlength",
+           y = "Weight (in gram)")
+  })
+}
+
+
+# Bring everything together -----------------------------------------------
+
+# Run the application
+shinyApp(ui = ui, server = server)
